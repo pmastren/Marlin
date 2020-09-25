1839a7600d22680ffd0b5aa669fcf8709a01cd82
Peter Mastren <peter@mastren.org>
2020-09-25 08:23:47 -0500

    Restore correct steps/mm
    Restore the correct steps per mm after getting TMC drivers UART mode working

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -753,7 +753,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 50, 50, 200, 203 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 407 }
 
 /**
  * Default Max Feed Rate (mm/s)
